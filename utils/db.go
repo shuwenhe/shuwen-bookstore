476b403872ac8beca8069eaa83e97312c223ec0d
shuwen he <645996795@qq.com>
2020-10-17 11:21:40 +0800

    fix db

--- utils/db.go
+++ utils/db.go
@@ -6,12 +6,10 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	Db  *sql.DB
-	err error
-)
+var Db *sql.DB
 
 func init() {
+	var err error
 	Db, err = sql.Open("mysql", "bookstore:Bookstore.com@_00..@tcp(62.234.11.179:3306)/bookstore")
 	if err != nil {
 		panic(err.Error())
